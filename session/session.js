91fc107d1b620651354f2e99c49f859fa141997e
Jeffery To <jeffery.to@gmail.com>
2010-11-17 19:03:46 +0800

    fixed a reference to an old name

--- session/session.js
+++ session/session.js
@@ -1,6 +1,6 @@
 steal.plugins('ss/model/html5_store/local').then(function($){
 
-  SS.Model.LocalStore.extend("SS.Model.HTML5Store.Session", {
+  SS.Model.HTML5Store.Local.extend("SS.Model.HTML5Store.Session", {
     storageMethod: "sessionStorage"
   });
   
